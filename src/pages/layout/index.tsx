4c88355b11e6ae99f3b8e38f9391c7960deec328
guofangchao <guofangchao@daling.com>
2019-10-30 10:47:34 +0800

    添加路由

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import './index.less';
 import Utils from '../../services/utils';
+import { Menu } from 'element-react';
+import url_configs from '../../routes/config';
+
+function RenderMenus(props: any) {
+    const item = props.item;
+    if (item.hide) return null;
+    return (
+        <Menu.Item index={item.path}>
+            {item.icon && <i className={item.icon}></i>}
+            {item.title}
+        </Menu.Item>
+    );
+}
 
 export default class extends React.Component<iReactRoute> {
     render() {
-        return this.props.children;
+        return (
+            <div>
+                <div id="sider">
+                    <div id="logo">后台系统 v1.0</div>
+                    <div id="menus">
+                        <Menu mode="vertical" onSelect={this.onSelect} defaultActive="/" theme="dark">
+                            {url_configs.map(item => (
+                                <RenderMenus key={item.name} item={item} />
+                            ))}
+                        </Menu>
+                    </div>
+                </div>
+                <div id="main">{this.props.children}</div>
+            </div>
+        );
     }
 
     componentDidMount() {
         console.log(Utils.checkUrl(this.props.location.pathname));
     }
+    componentWillUpdate() {
+        console.log('adw');
+    }
+
+    onSelect = (index: string) => {
+        console.log('select', index);
+    };
 }
--- src/routes/config.ts
+++ src/routes/config.ts
@@ -3,7 +3,7 @@ import Login from '../pages/login/index';
 import Err404 from '../pages/404/index';
 
 export default [
-    { name: 'home', path: '/', page: Home, exact: true },
-    { name: 'login', path: '/login', page: Login, exact: true },
-    { name: '404', path: '*', page: Err404, exact: true }
+    { name: 'home', title: '首页', icon: 'el-icon-message', path: '/', page: Home, exact: true },
+    { name: 'login', title: '登录', icon: 'el-icon-message', path: '/login', page: Login, exact: true },
+    { name: '404', path: '*', page: Err404, exact: true, hide: true }
 ];
