1a8a6f2f30dd5abeac84b42d9193ffb7021f355c
guofangchao <guofangchao@daling.com>
2019-10-30 14:50:42 +0800

    登录页

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import './index.less';
+import { Input, Button } from 'element-react';
 
-export default class extends React.Component {
+export default class extends React.Component<iReactRoute> {
     render() {
-        return <div>login</div>;
+        return (
+            <div id="login">
+                <div className="login_container">
+                    <div className="logo">
+                        AST<span>管理系统v1.0</span>
+                    </div>
+                    <div className="form">
+                        <div className="item">
+                            <Input prepend={<i className="fa fa-user"></i>} placeholder="请输入用户名" />
+                        </div>
+                        <div className="item">
+                            <Input prepend={<i className="fa fa-key"></i>} placeholder="请输入密码" />
+                        </div>
+                        <div className="item">
+                            <Button type="primary" onClick={this.login}>
+                                登 录<i className="fa fa-sign-in"></i>
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
+
+    login = () => {
+        this.props.history.push('/');
+    };
 }
