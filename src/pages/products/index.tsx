75c5a55312cf4befaf317452f569f638624a89a6
guofangchao <guofangchao@daling.com>
2019-10-30 20:24:40 +0800

    测试列表

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table, Pagination } from 'element-react';
+import { Table, Pagination, Button } from 'element-react';
 import './index.less';
 
 interface iState {
@@ -17,16 +17,53 @@ export default class extends React.Component<any, iState> {
         {
             label: 'id',
             prop: 'id',
-            width: 180
+            width: 90
+        },
+        {
+            label: '商品图',
+            prop: 'image',
+            width: 140,
+            render: (row: any) => {
+                return <img src={row.image} alt="" />;
+            }
         },
         {
             label: '标题',
             prop: 'title',
-            width: 180
+            width: 180,
+            render: (row: any) => {
+                return (
+                    <div>
+                        <div>{row.name}</div>
+                        <div>{row.title}</div>
+                    </div>
+                );
+            }
         },
         {
-            label: '地址',
+            label: '产地',
             prop: 'address'
+        },
+        {
+            label: '联系人',
+            prop: 'concat',
+            width: 90
+        },
+        {
+            label: '操作',
+            width: 180,
+            render: (row: any) => {
+                return (
+                    <Button.Group>
+                        <Button type="primary" size="small">
+                            上一页
+                        </Button>
+                        <Button type="primary" size="small">
+                            下一页
+                        </Button>
+                    </Button.Group>
+                );
+            }
         }
     ];
     render() {
@@ -45,7 +82,11 @@ export default class extends React.Component<any, iState> {
         for (let index = 0; index < 20; index++) {
             list.push({
                 id: index,
-                title: 'title' + index
+                title: 'title' + index,
+                name: 'name' + index,
+                concat: '郭方超',
+                address: '北京市',
+                image: 'http://img5.daling.com/data/files/zin/public/common/2019/10/17/14/31/45/AIKGUER000002239101.JPG_375x375.jpg'
             });
         }
         this.setState({ list });
